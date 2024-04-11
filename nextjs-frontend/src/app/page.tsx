f148e675f66fb1d2198e531ce538af2a65ff5865
Kasper Storgaard <kast@trifork.com>
2024-04-11 17:51:46 +0200

    test

--- nextjs-frontend/src/app/page.tsx
+++ nextjs-frontend/src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           priority
         />
       </div>
-
+<p>This is a test, to see if chatgbt can review it</p>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
