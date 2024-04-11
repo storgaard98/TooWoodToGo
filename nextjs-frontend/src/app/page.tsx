36f7ae177739e5894581de3b518784cdd8ab93ab
Kasper Storgaard <kast@trifork.com>
2024-04-11 17:59:19 +0200

    test code review

--- nextjs-frontend/src/app/page.tsx
+++ nextjs-frontend/src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
+
+function Home() {
+  let i = 0;
+  let k = 2;
+  let j = k +k +i+ i;
+  const h = j * (k + i);
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -27,6 +35,7 @@ export default function Home() {
           </a>
         </div>
       </div>
+      <div>cathc this</div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
@@ -38,7 +47,6 @@ export default function Home() {
           priority
         />
       </div>
-<p>This is a test, to see if chatgbt can review it</p>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
