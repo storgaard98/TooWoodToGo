6f7d979c66471538797ae4015fdeb8c9d8929e4e
Kasper Storgaard <kast@trifork.com>
2024-05-02 10:29:58 +0200

    Update metadata in layout.tsx and remove login page

--- nextjs-frontend/app/page.tsx
+++ nextjs-frontend/app/page.tsx
@@ -1,30 +1,21 @@
 "use client";
 // pages/index.tsx
 import React from "react";
+import LoginFormula from "./components/loginFormula";
+import Link from "next/link";
 
-
-import type { Metadata } from "next";
-
-import "./globals.css";
-
-// These styles apply to every route in the application
 
 const nodeUrl = process.env.NEXT_PUBLIC_NODE_URL;
 
-const HomePage = () => {
-  const nodeUrl = process.env.NEXT_PUBLIC_NODE_URL;
+const loginPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">Hej Frede</h1>
-      <button
-        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => alert("Undskyld, hvis jeg har lavet kæmpe kaos i dine filer, men tailwind virker")}
-      >
-        Tryk her
-      </button>
-
+   <div>
+      <LoginFormula />
+      <Link href="./SignUp">
+        Aren&rsquo;t you signed up yet? <b>Sign in</b>
+      </Link>
     </div>
   );
 };
 
-export default HomePage;
+export default loginPage;
