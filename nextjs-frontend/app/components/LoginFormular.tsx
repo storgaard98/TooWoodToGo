e3f80b12be5dabc547425711f81b9bb8deb6b244
au666766 <202004750@post.au.dk>
2024-05-06 12:43:30 +0200

    login med ikoner

---
+++ nextjs-frontend/app/components/LoginFormular.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const LoginFormular = () => {
+  return (
+    <>
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text text-white">Company Mail</span>
+        </div>
+        <div className="relative w-full max-w-xs">
+          <input
+            type="text"
+            //placeholder="Type here"
+            className="input w-full max-w-xs rounded-3px pr-8 pl-8"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="absolute top-1/2 left-2 transform -translate-y-1/2 w-4 h-4 opacity-50"
+          >
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
+        </div>
+      </label>
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text text-white">Password</span>
+        </div>
+        <div className="relative w-full max-w-xs">
+          <input
+            type="password"
+            //placeholder="Type here"
+            className="input w-full max-w-xs rounded-3px pr-8 pl-8"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="absolute top-1/2 left-2 transform -translate-y-1/2 w-4 h-4 opacity-50"
+          >
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </label>
+    </>
+  );
+};
+
+export default LoginFormular;
--- nextjs-frontend/app/page.tsx
+++ nextjs-frontend/app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 // pages/index.tsx
 import React from "react";
-import LoginFormula from "./components/loginFormula";
+import LoginFormula from "./components/LoginFormular";
 import Link from "next/link";
-
 
 const nodeUrl = process.env.NEXT_PUBLIC_NODE_URL;
 
 const loginPage = () => {
   return (
-   <div>
+    <div className="bg-stark-orange">
       <LoginFormula />
       <Link href="./SignUp">
         Aren&rsquo;t you signed up yet? <b>Sign in</b>
