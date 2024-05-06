54b13d947241bf2def94e223d62eee740aec5701
au666766 <202004750@post.au.dk>
2024-05-06 16:17:47 +0200

    Styling added + button. Need a redirection and a powered by STARK

--- nextjs-frontend/app/components/LoginFormular.tsx
+++ nextjs-frontend/app/components/LoginFormular.tsx
@@ -3,42 +3,42 @@ import React from "react";
 const LoginFormular = () => {
   return (
     <>
-      <label className="form-control w-full max-w-xs">
+      <label className="form-control w-full max-w-xl ">
         <div className="label">
-          <span className="label-text text-white">Company Mail</span>
+          <span className="label-text text-white text-4xl">Company Mail</span>
         </div>
-        <div className="relative w-full max-w-xs">
+        <div className="relative w-full max-w-xl">
           <input
             type="text"
             //placeholder="Type here"
-            className="input w-full max-w-xs rounded-3px pr-8 pl-8"
+            className="input w-full max-w-xl rounded-3px pr-8 pl-14 text-4xl h-24"
           />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="absolute top-1/2 left-2 transform -translate-y-1/2 w-4 h-4 opacity-50"
+            className="absolute top-1/2 left-3 transform -translate-y-1/2 w-8 h-8 opacity-50"
           >
             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
         </div>
       </label>
-      <label className="form-control w-full max-w-xs">
+      <label className="form-control w-full max-w-xl pt-11">
         <div className="label">
-          <span className="label-text text-white">Password</span>
+          <span className="label-text text-white text-4xl">Password</span>
         </div>
-        <div className="relative w-full max-w-xs">
+        <div className="relative w-full max-w-xl">
           <input
             type="password"
             //placeholder="Type here"
-            className="input w-full max-w-xs rounded-3px pr-8 pl-8"
+            className="input w-full max-w-xl rounded-3px pr-8 pl-14 text-4xl h-24"
           />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="absolute top-1/2 left-2 transform -translate-y-1/2 w-4 h-4 opacity-50"
+            className="absolute top-1/2 left-3 transform -translate-y-1/2 w-8 h-8 opacity-50"
           >
             <path
               fillRule="evenodd"
---
+++ nextjs-frontend/app/components/SignInButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInButton = () => {
+  return (
+    <button className="btn btn-outline border-white text-white hover:bg-white hover:bg-opacity-30 hover:border-white w-1/4 h-32 mt-40 text-4xl">
+      Sign In
+    </button>
+  );
+};
+
+export default SignInButton;
