430f90b86cf8fbab88986485f1463fcbf667b8d8
au666766 <202004750@post.au.dk>
2024-05-07 14:07:04 +0200

    Sign In page done

--- nextjs-frontend/app/components/SignInButton.tsx
+++ nextjs-frontend/app/components/SignInButton.tsx
@@ -1,8 +1,19 @@
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const SignInButton = () => {
+  const router = useRouter();
+
+  const goToHomePage = () => {
+    router.push("/home");
+    console.log("Click");
+  };
+
   return (
-    <button className="btn btn-outline border-white text-white hover:bg-white hover:bg-opacity-30 hover:border-white w-1/4 h-32 mt-40 text-4xl">
+    <button
+      onClick={goToHomePage}
+      className="btn btn-outline border-white text-white hover:bg-white hover:bg-opacity-30 hover:border-white w-1/4 h-32 mt-40 text-4xl"
+    >
       Sign In
     </button>
   );
