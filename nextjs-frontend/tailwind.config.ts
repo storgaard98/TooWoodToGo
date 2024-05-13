732f839e87cece42c95579ac4503ab395b47460b
Kasper Storgaard <kast@trifork.com>
2024-05-13 15:17:23 +0200

    test for deployment

--- nextjs-frontend/tailwind.config.ts
+++ nextjs-frontend/tailwind.config.ts
@@ -35,19 +35,6 @@ const config: Config = {
         "45px": "45px",
         "70px": "70px",
       },
-
-      height:{
-        "16/17": "94.118%",
-        "600px": "600px",
-      },
-
-      padding:{
-        "205px": "205px",
-        "500px": "500px",
-        "2080px": "2080px",
-        "2528px": "2528px",
-      },
-
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
