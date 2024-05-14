e0d4aa9843a32e21cde163321332e0362a5241af
storgaard98 <79849941+storgaard98@users.noreply.github.com>
2024-05-14 21:30:53 +0200

    Db func (#23)
    * recorder now working
    
    * ping db
    
    * db test
    
    * Added btn to save recording, due to failure
    
    * db connection
    
    * help to create db connection
    
    * backlog update
    
    * remove package
    
    * db now working
    
    * uniqueID in mongo
    
    * Added TODO
    
    * fix
    
    ---------
    
    Co-authored-by: Kasper Storgaard <kast@trifork.com>

--- nextjs-frontend/app/home/components/audioRecorder.tsx
+++ nextjs-frontend/app/home/components/audioRecorder.tsx
@@ -1,6 +1,4 @@
 // AudioRecorder.tsx
-"useState";
-
 import React, { useState, useRef } from "react";
 
 interface AudioRecorderProps {
@@ -35,29 +33,30 @@ const AudioRecorder: React.FC<AudioRecorderProps> = ({ onSaveRecording }) => {
     }
   };
 
-  const stopRecording = () => {
+  const stopRecording = async () => {
     if (mediaRecorderRef.current && audioStreamRef.current) {
       mediaRecorderRef.current.stop();
       audioStreamRef.current.getTracks().forEach((track) => track.stop());
       setRecording(false);
-      saveRecording();
     }
   };
 
   const saveRecording = () => {
-    const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+    if (audioChunks.length === 0) return console.log("No recording to save");
+    const audioBlob = new Blob(audioChunks, { type: "audio/mp4" });
     const audioUrl = URL.createObjectURL(audioBlob);
     onSaveRecording(audioBlob); // Call the parent component's function with the audio URL
     console.log("Recording saved:", audioUrl);
+    return;
   };
 
-  const deleteRecording = () => {
+  const deleteRecording = async () => {
     setAudioChunks([]);
-    stopRecording();
+    saveRecording();
   };
 
   const playRecording = () => {
-    const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+    const audioBlob = new Blob(audioChunks, { type: "audio/mp4" });
     const audioUrl = URL.createObjectURL(audioBlob);
     const audio = new Audio(audioUrl);
     console.log("Playing recording:", audioUrl);
@@ -66,28 +65,51 @@ const AudioRecorder: React.FC<AudioRecorderProps> = ({ onSaveRecording }) => {
 
   return (
     <div>
-      <button
-        type="button"
-        onClick={(event) => {
-          event.stopPropagation();
-          recording ? stopRecording() : startRecording();
-          recording ? setButtonShowed(true) : setButtonShowed(false);
-        }}
-        className={`btn h-auto border-none bg-input-box-blue border-transparent rounded-2xl shadow-md hover:bg-stark-orange transition duration-200 ease-in-out ${recording ? "bg-stark-orange" : "bg-input-box-blue"} `}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="70"
-          height="70"
-          viewBox="0 0 44 44"
-          fill="none"
+      {audioChunks.length < 1 ? (
+        <button
+          type="button"
+          onClick={async (event) => {
+            event.preventDefault();
+            recording ? stopRecording() : startRecording();
+            recording ? setButtonShowed(true) : setButtonShowed(false);
+          }}
+          className={`btn h-auto border-none bg-input-box-blue border-transparent rounded-2xl shadow-md hover:bg-stark-orange transition duration-200 ease-in-out ${recording ? "bg-stark-orange" : "bg-input-box-blue"} `}
         >
-          <path
-            d="M22 30.25C24.1873 30.2477 26.2844 29.3778 27.8311 27.8311C29.3778 26.2844 30.2477 24.1873 30.25 22V11C30.25 8.81196 29.3808 6.71354 27.8336 5.16637C26.2865 3.61919 24.188 2.75 22 2.75C19.812 2.75 17.7135 3.61919 16.1664 5.16637C14.6192 6.71354 13.75 8.81196 13.75 11V22C13.7523 24.1873 14.6222 26.2844 16.1689 27.8311C17.7156 29.3778 19.8127 30.2477 22 30.25ZM16.5 11C16.5 9.54131 17.0795 8.14236 18.1109 7.11091C19.1424 6.07946 20.5413 5.5 22 5.5C23.4587 5.5 24.8576 6.07946 25.8891 7.11091C26.9205 8.14236 27.5 9.54131 27.5 11V22C27.5 23.4587 26.9205 24.8576 25.8891 25.8891C24.8576 26.9205 23.4587 27.5 22 27.5C20.5413 27.5 19.1424 26.9205 18.1109 25.8891C17.0795 24.8576 16.5 23.4587 16.5 22V11ZM23.375 35.6813V41.25C23.375 41.6147 23.2301 41.9644 22.9723 42.2223C22.7144 42.4801 22.3647 42.625 22 42.625C21.6353 42.625 21.2856 42.4801 21.0277 42.2223C20.7699 41.9644 20.625 41.6147 20.625 41.25V35.6813C17.235 35.3363 14.0933 33.7466 11.8075 31.2195C9.52174 28.6924 8.25419 25.4075 8.25 22C8.25 21.6353 8.39487 21.2856 8.65273 21.0277C8.91059 20.7699 9.26033 20.625 9.625 20.625C9.98967 20.625 10.3394 20.7699 10.5973 21.0277C10.8551 21.2856 11 21.6353 11 22C11 24.9174 12.1589 27.7153 14.2218 29.7782C16.2847 31.8411 19.0826 33 22 33C24.9174 33 27.7153 31.8411 29.7782 29.7782C31.8411 27.7153 33 24.9174 33 22C33 21.6353 33.1449 21.2856 33.4027 21.0277C33.6606 20.7699 34.0103 20.625 34.375 20.625C34.7397 20.625 35.0894 20.7699 35.3473 21.0277C35.6051 21.2856 35.75 21.6353 35.75 22C35.7458 25.4075 34.4783 28.6924 32.1925 31.2195C29.9067 33.7466 26.765 35.3363 23.375 35.6813Z"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="70"
+            height="70"
+            viewBox="0 0 44 44"
+            fill="none"
+          >
+            <path
+              d="M22 30.25C24.1873 30.2477 26.2844 29.3778 27.8311 27.8311C29.3778 26.2844 30.2477 24.1873 30.25 22V11C30.25 8.81196 29.3808 6.71354 27.8336 5.16637C26.2865 3.61919 24.188 2.75 22 2.75C19.812 2.75 17.7135 3.61919 16.1664 5.16637C14.6192 6.71354 13.75 8.81196 13.75 11V22C13.7523 24.1873 14.6222 26.2844 16.1689 27.8311C17.7156 29.3778 19.8127 30.2477 22 30.25ZM16.5 11C16.5 9.54131 17.0795 8.14236 18.1109 7.11091C19.1424 6.07946 20.5413 5.5 22 5.5C23.4587 5.5 24.8576 6.07946 25.8891 7.11091C26.9205 8.14236 27.5 9.54131 27.5 11V22C27.5 23.4587 26.9205 24.8576 25.8891 25.8891C24.8576 26.9205 23.4587 27.5 22 27.5C20.5413 27.5 19.1424 26.9205 18.1109 25.8891C17.0795 24.8576 16.5 23.4587 16.5 22V11ZM23.375 35.6813V41.25C23.375 41.6147 23.2301 41.9644 22.9723 42.2223C22.7144 42.4801 22.3647 42.625 22 42.625C21.6353 42.625 21.2856 42.4801 21.0277 42.2223C20.7699 41.9644 20.625 41.6147 20.625 41.25V35.6813C17.235 35.3363 14.0933 33.7466 11.8075 31.2195C9.52174 28.6924 8.25419 25.4075 8.25 22C8.25 21.6353 8.39487 21.2856 8.65273 21.0277C8.91059 20.7699 9.26033 20.625 9.625 20.625C9.98967 20.625 10.3394 20.7699 10.5973 21.0277C10.8551 21.2856 11 21.6353 11 22C11 24.9174 12.1589 27.7153 14.2218 29.7782C16.2847 31.8411 19.0826 33 22 33C24.9174 33 27.7153 31.8411 29.7782 29.7782C31.8411 27.7153 33 24.9174 33 22C33 21.6353 33.1449 21.2856 33.4027 21.0277C33.6606 20.7699 34.0103 20.625 34.375 20.625C34.7397 20.625 35.0894 20.7699 35.3473 21.0277C35.6051 21.2856 35.75 21.6353 35.75 22C35.7458 25.4075 34.4783 28.6924 32.1925 31.2195C29.9067 33.7466 26.765 35.3363 23.375 35.6813Z"
+              fill="#007BFF"
+            />
+          </svg>
+        </button>
+      ) : (
+        //TODO Our new beautiful button, should be styled
+        <button
+          type="button"
+          onClick={(event) => {
+            event.preventDefault();
+            saveRecording();
+          }}
+          className={`btn h-auto border-0 bg-input-box-blue border-transparent rounded-3xl shadow-md hover:bg-stark-orange transition duration-200 ease-in-out ${recording ? "bg-stark-orange" : "bg-input-box-blue"} transform -translate-y-6`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="150"
+            height="150"
+            viewBox="0 0 24 24"
             fill="#007BFF"
-          />
-        </svg>
-      </button>
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M6 3h12l1.707 1.707L21 7v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm1 2v4h10V5H7zm2 6v2h2v-2H9zm2 3v2h2v-2h-2z" />
+          </svg>
+        </button>
+      )}
       <div
         className={`flex flex-row space-x-5 transform transition-opacity ease-in-out duration-700 ${isButtonShowed ? "opacity-100" : "opacity-0"}`}
       >
